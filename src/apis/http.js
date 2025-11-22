c90eb562715f1e6b06519e26a9580b1144dcd259
wonpark1 <2023111927@dgu.ac.kr>
2025-11-22 22:03:33 +0900

    init: axios 초기세팅

---
+++ src/apis/http.js
@@ -0,0 +1,21 @@
+import axiosInstance from "./axiosInstance";
+
+export const get = async (...args) => {
+  try {
+    const response = await axiosInstance.get(...args);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+export const post = async (url, body, config) => {
+  try {
+    const response = await axiosInstance.post(url, body, config);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
